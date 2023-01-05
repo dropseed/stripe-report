ab18324dd39f532272e431001f9be5ae047166b3
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-01-05 15:50:33 -0600

    Refactor

--- main.py
+++ main.py
@@ -18,11 +18,20 @@ EVENT_TYPES = (
     "invoice.payment_succeeded",
 )
 
+EVENT_ICONS = {
+    "customer.subscription.created": "🚀",
+    "invoice.payment_succeeded": "💵",
+}
+
 
 class StripeReporter:
-    def __init__(self):
-        self.account_keys = {}
+    def __init__(self, account_keys: dict):
+        self.account_keys = account_keys
         self.results = {}
+
+    @property
+    def has_results(self):
+        return any(self.results.values())
 
     def run(self):
         self.results = {}
@@ -31,6 +40,7 @@ class StripeReporter:
             self.results[name] = self.events_for_key(key) + self.invoices_for_key(key)
 
     def events_for_key(self, stripe_key):
+        """Get any events in the last 24 hours"""
         ago = datetime.datetime.now() - datetime.timedelta(days=1)
 
         stripe.api_key = stripe_key
@@ -50,11 +60,7 @@ class StripeReporter:
                 amount = f"${amount:.2f}"
             customer_email = obj.get("customer_email", "")
 
-            icon = ""
-            if event["type"] == "customer.subscription.created":
-                icon = "🚀"
-            elif event["type"] == "invoice.payment_succeeded":
-                icon = "💵"
+            icon = EVENT_ICONS.get(event["type"], "")
 
             s = f"{icon} <a href='https://dashboard.stripe.com/events/{event['id']}'>{event['type']}</a>: {customer_email} {amount}"
             output.append(s)
@@ -62,6 +68,7 @@ class StripeReporter:
         return output
 
     def invoices_for_key(self, stripe_key):
+        """Get any open invoices"""
         stripe.api_key = stripe_key
         invoices = list(stripe.Invoice.list(limit=100, status="open"))
 
@@ -75,15 +82,19 @@ class StripeReporter:
 
         return output
 
+    def print_results(self):
+        print(json.dumps(reporter.results, indent=2))
 
-def email_results(results):
-    html =  "<h2>Dropseed Stripe report for the last 24 hrs</h2>"
-    for name, lines in results.items():
-        if lines:
-            html += f"<h3>{name}</h3>"
-            html += "<ul>" + "".join([f"<li>{x}</li>" for x in lines]) + "</ul>"
+    def email_results(self):
+        html = "<h2>Stripe report for the last 24 hrs</h2>"
+        for name, lines in self.results.items():
+            if lines:
+                html += f"<h3>{name}</h3>"
+                html += "<ul>" + "".join([f"<li>{x}</li>" for x in lines]) + "</ul>"
 
-    send_email("Dropseed Stripe Report", html, os.environ["EMAIL"], "Dropseed Reports <reports@dropseed.io>")
+        send_email(
+            "Stripe Report", html, os.environ["TO_EMAIL"], os.environ["FROM_EMAIL"]
+        )
 
 
 def send_email(subject, html, to_email, from_email):
@@ -92,11 +103,11 @@ def send_email(subject, html, to_email, from_email):
     from email.mime.multipart import MIMEMultipart
 
     msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
 
-    msg.attach(MIMEText(html, 'html'))
+    msg.attach(MIMEText(html, "html"))
 
     mailServer = smtplib.SMTP(os.environ["SMTP_HOST"], os.environ.get("SMTP_PORT", 587))
     mailServer.ehlo()
@@ -107,17 +118,22 @@ def send_email(subject, html, to_email, from_email):
     mailServer.close()
 
 
+def stripe_keys_from_env():
+    stripe_keys = {}
+
+    # Load keys from environment (STRIPE_KEY_{name}=key)
+    for v in os.environ:
+        if v.startswith("STRIPE_KEY_"):
+            name = v[11:]
+            key = os.environ[v]
+            stripe_keys[name] = key
+
+    return stripe_keys
+
+
 if __name__ == "__main__":
-    reporter = StripeReporter()
-
-    # name=key name=key etc
-    stripe_input = os.environ["STRIPE_ACCOUNTS"]
-    parts = stripe_input.split()
-    for part in parts:
-        name, key = part.split("=")
-        reporter.account_keys[name] = key
-
+    reporter = StripeReporter(account_keys=stripe_keys_from_env())
     reporter.run()
-    print(json.dumps(reporter.results, indent=2))
-    if any(reporter.results.values()):
-        email_results(reporter.results)
+    reporter.print_results()
+    if reporter.has_results:
+        reporter.email_results()
